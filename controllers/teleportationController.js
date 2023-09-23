1aad08d3d52161f76bdf4c37c48c5936566f8ac9
vijay <rithikvj@gmail.com>
2023-09-24 03:33:19 +0800

    Update teleportationController.js

--- controllers/teleportationController.js
+++ controllers/teleportationController.js
@@ -1,4 +1,19 @@
-// controllers/teleportationController.js
+// Validate that the input arrays are not empty and contain valid coordinates
+function isValidInput(k, p, q) {
+    if (!Array.isArray(p) || !Array.isArray(q) || typeof k !== "number") {
+        return false;
+    }
+    for (const point of [...p, ...q]) {
+        if (
+            !Array.isArray(point) ||
+            point.length !== 2 ||
+            !point.every(Number.isFinite)
+        ) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function euclideanDistance([x1, y1], [x2, y2]) {
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
@@ -23,6 +38,12 @@ function findMinimumDistance(k, p, q) {
 
 exports.calculateTeleportation = (req, res) => {
     const { k, p, q } = req.body;
+
+    if (!isValidInput(k, p, q)) {
+        res.status(400).send("Invalid input");
+        return;
+    }
+
     const result = findMinimumDistance(k, p, q);
     res.set("Content-Type", "text/plain");
     res.send(result);
