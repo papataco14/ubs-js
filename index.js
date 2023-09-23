f091726a8fdac7fdc3ca767358f56f3f170c5993
vijay <rithikvj@gmail.com>
2023-09-24 03:11:17 +0800

    Add log tracking

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const morganBody = require("morgan-body");
 const routes = require("./routes");
+const fs = require("fs");
+
 const PORT = process.env.PORT || 5000;
 
+// Clear the log file when the application starts
+fs.writeFileSync("request-response-log.txt", "");
+
 const app = express().use(express.json());
-morganBody(app, { noColors: process.env.NODE_ENV === "production" });
+
+// Middleware to log request body and corresponding response
+app.use((req, res, next) => {
+    let oldWrite = res.write,
+        oldEnd = res.end;
+
+    let chunks = [];
+
+    res.write = function (chunk) {
+        chunks.push(chunk);
+        oldWrite.apply(res, arguments);
+    };
+
+    res.end = function (chunk) {
+        if (chunk) chunks.push(chunk);
+        let body = Buffer.concat(chunks).toString("utf8");
+
+        // Log request and response
+        fs.appendFileSync(
+            "request-response-log.txt",
+            `Request Body:\n${JSON.stringify(
+                req.body
+            )}\nResponse Body:\n${body}\n---\n`
+        );
+
+        oldEnd.apply(res, arguments);
+    };
+
+    next(); // Proceed to the next middleware
+});
 
 // Use routes
 app.use("/", routes);
