f425351d6f87c61658a88059a00392cec500a7bd
vijay <rithikvj@gmail.com>
2023-09-24 02:58:18 +0800

    Update teleportationController.js

--- controllers/teleportationController.js
+++ controllers/teleportationController.js
@@ -1,38 +1,29 @@
-function euclideanDistance(point1, point2) {
-    return Math.sqrt(
-        Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2)
-    );
+// controllers/teleportationController.js
+
+function euclideanDistance([x1, y1], [x2, y2]) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
-function findMinimumDistance(input) {
-    const { k, p, q } = input;
+function findMinimumDistance(k, p, q) {
     let totalDistance = 0;
-    let distances = [];
 
-    for (const deliveryLocation of q) {
+    for (const deliveryPoint of q) {
         let minDistance = Infinity;
 
-        for (const hub of p) {
-            const distance = euclideanDistance(deliveryLocation, hub);
+        for (const teleportPoint of p) {
+            const distance = euclideanDistance(deliveryPoint, teleportPoint);
             minDistance = Math.min(minDistance, distance);
         }
 
-        distances.push(minDistance);
-    }
-
-    // Sort the distances to use teleportation orbs for the k largest delivery locations
-    distances.sort((a, b) => b - a);
-
-    for (let i = k; i < distances.length; i++) {
-        totalDistance += distances[i];
+        totalDistance += minDistance;
     }
 
     return totalDistance.toFixed(2);
 }
 
 exports.calculateTeleportation = (req, res) => {
-    const inputData = req.body;
-    const outputData = findMinimumDistance(inputData);
-    res.setHeader("Content-Type", "text/plain");
-    res.send(outputData);
+    const { k, p, q } = req.body;
+    const result = findMinimumDistance(k, p, q);
+    res.set("Content-Type", "text/plain");
+    res.send(result);
 };
